3c2568fb18d707c9f48e7f9da8fae9b85be2ba58
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2017-11-03 09:02:45 -0700

    Added survey and friends

---
+++ app/data/friends.js
@@ -0,0 +1,94 @@
+
+var friends = [
+
+    {
+      'name': "Cookaboo",
+      'photo': 'photo link goes here',
+      'scores': [
+          5,
+          4,
+          3,
+          2,
+          1,
+          1,
+          2,
+          1,
+          4,
+          1
+      ],
+    },
+
+    {
+        'name': "User 2",
+        'photo': 'photo link goes here',
+        'scores': [
+            1,
+            1,
+            1,
+            3,
+            4,
+            4,
+            5,
+            2,
+            1,
+            1
+        ],
+      },
+      
+      {
+        'name': "User 3",
+        'photo': 'photo link goes here',
+        'scores': [
+            1,
+            1,
+            1,
+            3,
+            4,
+            4,
+            5,
+            2,
+            1,
+            1
+        ],
+      },
+
+      {
+        'name': "User 4",
+        'photo': 'photo link goes here',
+        'scores': [
+            1,
+            1,
+            1,
+            5,
+            5,
+            5,
+            5,
+            1,
+            1,
+            1
+        ],
+      },
+
+      {
+        'name': "User 5",
+        'photo': 'photo link goes here',
+        'scores': [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ],
+      }
+
+];
+
+  
+  // Export array
+  module.exports = friends;
+  
